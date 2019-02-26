cc2d5077110a1795ab89a2cd96ec23ad9eef4e8e
Ernesto Pena <ernestopenad@gmail.com>
2019-02-25 21:12:50 -0600

    Scheduler App back end user interface lay out

---
+++ src/components/nav-bar.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Navigation extends Component {
+    status = {}
+    render() {
+        return (
+            <React.Fracment>
+                <h1>This is an example</h1>
+
+
+
+            </React.Fracment>
+        );
+    }
+}
+
+export default Navigation;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+import './index.css';
+import App from "./app.js";
+import 'bootstrap/dist/css/bootstrap.css'
+import Counter from './components/counter.jsx';
+import Navigation from './components/nav-bar.jsx';
+
+
+
+//ReactDOM.render(<Counter/>, document.getElementById('root'));
+
